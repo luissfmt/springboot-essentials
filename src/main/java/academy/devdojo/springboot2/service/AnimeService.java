4ea896a1175ab8568e2f92dd71a7efb6455d9908
Luis Felipe Melo Tavares <luis.tavares@srconsulting.io>
2023-10-30 14:40:29 -0300

    método post

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -6,12 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 //  private final AnimeRepository animeRepository;
-  private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+  private static List<Anime> animes;
+  static {
+    animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk")));
+  }
+
   public List<Anime> listAll() {
     return animes;
   }
@@ -21,5 +27,13 @@ public class AnimeService {
             .filter(anime -> anime.getId().equals(id))
             .findFirst()
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+  }
+
+  public Anime save(Anime anime) {
+    anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+
+    animes.add(anime);
+
+    return anime;
   }
 }
