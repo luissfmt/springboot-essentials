8fd74b7a74885f367e58e1a8d14c250cc65156e3
Luis Felipe Melo Tavares <luis.tavares@srconsulting.io>
2023-10-31 17:36:34 -0300

    método findByName com request params

--- target/generated-sources/annotations/academy/devdojo/springboot2/mapper/AnimeMapperImpl.java
+++ target/generated-sources/annotations/academy/devdojo/springboot2/mapper/AnimeMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-31T14:52:28-0300",
+    date = "2023-10-31T17:29:32-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
 )
 @Component
