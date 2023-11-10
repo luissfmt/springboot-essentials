91babca1bcb2c7e8058b9b73d7185aee6c28ead9
Luis Felipe Melo Tavares <luis.tavares@srconsulting.io>
2023-11-10 11:17:10 -0300

    testes integrados animeController

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -3,11 +3,15 @@ package academy.devdojo.springboot2.requests;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
   @NotBlank(message = "The anime name cannot be blank or null")
   private String name;
