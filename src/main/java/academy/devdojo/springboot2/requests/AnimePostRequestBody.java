10034f924822c6e87b7ee7a28cd87c5e1b91e2ca
Luis Felipe Melo Tavares <luis.tavares@srconsulting.io>
2023-11-17 10:57:39 -0300

    documentação de api com spring doc openapi

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,5 +1,6 @@
 package academy.devdojo.springboot2.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -14,5 +15,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AnimePostRequestBody {
   @NotBlank(message = "The anime name cannot be blank or null")
+  @Schema(description = "This is the anime's name", example = "Tensei Shittara Slime Datta Ken")
   private String name;
 }
